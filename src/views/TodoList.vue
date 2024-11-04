9ae80d2f01f1c7ed415a7923ddc2067685d8bf98
Thắng Trần Minh <84889893+tranminhthang1606@users.noreply.github.com>
2024-11-04 09:55:53 +0700

    css background

--- src/views/TodoList.vue
+++ src/views/TodoList.vue
@@ -14,10 +14,23 @@ function changeTab(tab) {
 </script>
 
 <template>
-    <div class="bg-gray-100 min-h-screen flex items-center justify-center p-3">
+    <div class="bg-gray-100 main min-h-screen flex items-center justify-center p-3">
         <SideBar @changeTab="changeTab" />
         <MainSide :currentTab="currentTab" />
     </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+@keyframes gradientAnimation {
+    0% { background-position: 0% 50%; }
+    50% { background-position: 100% 50%; }
+    100% { background-position: 0% 50%; }
+}
+
+.main {
+    background: linear-gradient(270deg, #ff6f91, #ff9671, #ffc75f, #f9f871);
+    background-size: 400% 400%;
+    animation: gradientAnimation 8s ease infinite;
+}
+
+</style>
